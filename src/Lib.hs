a2e3943290bc91174d602e47b6500d65ab1f6273
Om Prakaash <omsuseela@gmail.com>
2023-11-23 17:09:47 -0800

    Implement rudiementary async requests

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,12 +8,18 @@ import System.TimeIt
 
 import Control.Parallel.Strategies
 import Control.Monad
+import Control.Concurrent.Async
+import Data.ByteString.Lazy
+import Data.Time.Clock (getCurrentTime, diffUTCTime, NominalDiffTime)
 
-data AttackResult = AttackResult { code :: Int, latency :: Double } deriving (Show)
+data AttackResult = AttackResult { code :: Int, latency :: NominalDiffTime } deriving (Show)
 
-attack :: String -> IO AttackResult
+
+-- wait for respoinse
+attack :: String -> IO (AttackResult)
 attack target = do
-    (duration, c) <- timeItT (callTarget target)
+    asyncResponse <- callTargetAsync target
+    (c, duration) <- wait asyncResponse
     return (AttackResult c duration)
 
 callTarget :: String -> IO Int
@@ -25,13 +31,32 @@ callTarget target = do
     let c = statusCode (responseStatus response)
     return c
 
+-- Async callTarget
+callTargetAsync :: String -> IO (Async (Int, NominalDiffTime))
+callTargetAsync target = do
+    manager <- newManager tlsManagerSettings
+    request <- parseRequest target
+    let requestWithUA = request { requestHeaders = [("User-Agent", "roebling")] }
+    startTime <- getCurrentTime
+    async $ do
+        response <- httpLbs requestWithUA manager
+        let c = statusCode (responseStatus response)
+        endTime <- getCurrentTime
+        let duration = endTime `diffUTCTime` startTime
+        return (c, duration)
+
+fetchSameUrlMultipleTimes :: String -> Int -> IO [AttackResult]
+fetchSameUrlMultipleTimes url count = do
+    replicateM count (attack url)
+
+
 printAttackResult :: AttackResult -> IO ()
 printAttackResult (AttackResult c d) = putStrLn $ "Status code: " ++ show c ++ ", latency: " ++ show d
 
-
+numAttacks::Int
 numAttacks = 100
 
 attacker :: String -> IO ()
 attacker target = do
-    attacks <- replicateM numAttacks (attack target >>= printAttackResult)
-    return ()
+    responses <- fetchSameUrlMultipleTimes target numAttacks
+    mapM_  printAttackResult responses
