f7cce1e7614bb3567a4ffc49357b8765914cdd49
Rohan Mishra <r2mishra@ucsd.edu>
2023-12-01 17:58:26 -0800

    constant targeter

--- src/Pacer.hs
+++ src/Pacer.hs
@@ -4,21 +4,23 @@ module Pacer
     pace,
   )
 where
+
 import Data.Time
 
-data PaceConfig = PaceConfig { rate :: Int, duration :: NominalDiffTime } deriving (Show)
-data PacerResult = PacerResult { stop :: Bool, waitTime :: NominalDiffTime } deriving (Show)
+data PaceConfig = PaceConfig {rate :: Int, duration :: NominalDiffTime} deriving (Show)
+
+data PacerResult = PacerResult {stop :: Bool, waitTime :: NominalDiffTime} deriving (Show)
 
 pace :: UTCTime -> Int -> PaceConfig -> IO PacerResult
 pace began hitCount config = do
   now <- getCurrentTime
   let elapsed = now `diffUTCTime` began
   if elapsed > duration config
-    then return PacerResult { stop = True, waitTime = 0 }
+    then return PacerResult {stop = True, waitTime = 0}
     else do
       let expectedHits = round (fromIntegral (rate config) * elapsed)
       if hitCount < expectedHits
-        then return PacerResult { stop = False, waitTime = 0 }
+        then return PacerResult {stop = False, waitTime = 0}
         else do
           let sleepTime = (fromIntegral (rate config) * fromIntegral (hitCount + 1)) - elapsed
-          return PacerResult { stop = False, waitTime = sleepTime }
+          return PacerResult {stop = False, waitTime = sleepTime}
