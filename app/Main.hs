f2a43ce88017d543d0daa25362650cfbffcbf056
Sumanth R Hegde <sumanthrh99@gmail.com>
2023-12-02 22:20:08 -0800

    added params and latency widgets

--- app/Main.hs
+++ app/Main.hs
@@ -2,31 +2,42 @@
 
 module Main (main) where
 
-import Args
 import Data.Text (unpack)
-import Lib
 import Options.Applicative
 import Data.Text (unpack)
+import Data.Time (NominalDiffTime)
 import Chart (Options(..), getPlotLines)
 import Control.Monad (when)
-import Brick.Widgets.Border.Style (unicode)
 
+import Brick.Widgets.Border.Style (unicode)
 import Brick
 import Brick.Widgets.Border
+
+import Lib
+import qualified Widgets as W
+import Args
+
 
 -- Sample data for the chart
 mySeries :: [Integer]
 mySeries = [1..20]
 
+myLatencies :: [NominalDiffTime]
+myLatencies = map fromRational [0.8, 0.7, 0.98, 0.55, 0.66]
+
 myoptions :: Options 
 myoptions  = MkOptions { height = 14 }
 
--- The UI widget that includes the ASCII chart
-ui :: Widget ()
-ui = joinBorders $
+plotWidget :: Widget n
+plotWidget = joinBorders $
     withBorderStyle unicode $
-    borderWithLabel (Brick.str "Hello") $
+    borderWithLabel (Brick.str "Plot") $
     Brick.str (unlines $ getPlotLines myoptions mySeries)
+
+-- The UI widget that includes the ASCII chart
+ui :: W.ParamsText -> [NominalDiffTime] -> Widget ()
+ui params latencies = vBox [plotWidget, hBox [W.drawParams params, W.drawLatencyStats latencies]]
+
 
 main :: IO ()
 main = do
@@ -34,7 +45,13 @@ main = do
     print cmdFlags
 
     when (plotDemo cmdFlags) $ do
-        simpleMain ui
+        let params = W.MakeParams {
+            W.target = target cmdFlags, 
+            W.rate = rate cmdFlags,
+            W.duration = duration cmdFlags,
+            W.method = method cmdFlags
+        }
+        simpleMain $ ui params myLatencies
 
     attacker (unpack $ target cmdFlags)
     putStrLn $ "Attacking " ++ show (target cmdFlags)
