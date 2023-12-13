410275c01be314b517e538f456e5bb446d5b9f47
Om Prakaash <omsuseela@gmail.com>
2023-12-12 18:02:58 -0800

    Fix ResulLogger

--- src/Attacker/ResultLogger.hs
+++ src/Attacker/ResultLogger.hs
@@ -11,18 +11,20 @@ import Utils.Models (AttackResult (..), AttackResultMessage (..))
 -- Dummy file to experiment with channels
 runLogger :: Chan AttackResultMessage -> IO ()
 runLogger channel = do
-  loop Nothing
-  where
-    loop msg = do
-      res <- readChan channel
-      case res of
-        StopMessage hitCount -> do
-          print $ "Logger ==> Will stop at Hit: " ++ show hitCount
-          loop $ Just (hitCount - 1)
-        ResultMessage (AttackResult hitCount code latency) -> do
-          if msg /= Just (hitCount + 1)
-            then do
-              print $ "Logger ==> Hit: " ++ show hitCount ++ ", Code: " ++ show code ++ ", Latency: " ++ show latency
-              loop msg
-            else do
-              return ()
+ loop Nothing 0 
+ where
+   loop :: Maybe Int -> Int -> IO ()
+   loop msg totHits = do
+     res <- readChan channel
+     case res of
+       StopMessage hitCount -> do
+         print $ "Logger ==> Will stop when totHits reaches: " ++ show hitCount
+         loop (Just (hitCount - 1)) totHits
+       ResultMessage (AttackResult hitCount code latency) -> do
+         if totHits /= hitCount + 1
+           then do
+             print $ "Logger ==> Hit: " ++ show hitCount ++ ", Code: " ++ show code ++ ", Latency: " ++ show latency
+             loop msg (totHits + 1)
+           else do
+             return ()
+
