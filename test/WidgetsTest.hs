8362136ab93f2332c40a3793c54e1a574aaf94eb
rajnawal <58648450+rajnawal@users.noreply.github.com>
2023-12-12 16:52:50 -0800

    Added Testing  (#15)
    * commented out logger test since it is not playing well right now; I suspect there is some issue with threading at play
    
    * Update test/WidgetsTest.hs
    
    * tried modifying test, still errorsome
    
    * fixed tests to use new modules

---
+++ test/WidgetsTest.hs
@@ -0,0 +1,23 @@
+module WidgetsTest(tests) where
+
+import qualified Test.Tasty as T
+import qualified Test.Tasty.HUnit as HU
+
+import qualified GUI.Widgets as W
+
+
+tests :: T.TestTree
+tests = T.testGroup "Widgets Tests" [
+    HU.testCase "Percentile" percentileTest,
+    HU.testCase "Get int rank percentile" getIntRankPercentileTest,
+    HU.testCase "Get frac rank percentile" getFracRankPercentileTest
+    ]
+
+percentileTest :: HU.Assertion
+percentileTest = HU.assertEqual "Percentile" 2.5 (W.percentile 67 [1.0, 2.0, 3.0])
+
+getIntRankPercentileTest :: HU.Assertion
+getIntRankPercentileTest = HU.assertEqual "Get int rank percentile" 2.0 (W.getIntRankPercentile 1.0 [1.0, 2.0, 3.0])
+
+getFracRankPercentileTest :: HU.Assertion
+getFracRankPercentileTest = HU.assertEqual "Get frac rank percentile" 2.0 (W.getFracRankPercentile 1.8 [1.0, 2.0, 3.0])
